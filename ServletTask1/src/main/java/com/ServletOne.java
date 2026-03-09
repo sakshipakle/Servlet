2a88445391f0a546edafcb6cf5f60d72b010f129
sakshipakle <sakshipakle1@gmail.com>
2026-03-09 17:48:14 +0530

    servlet tasks

---
+++ ServletTask1/src/main/java/com/ServletOne.java
@@ -0,0 +1,28 @@
+package com;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/ServletOne")
+public class ServletOne extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+   
+    public ServletOne() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter pw = response.getWriter();
+		
+		String name = request.getParameter("username");
+		pw.println("Welcome " +name + " to servlet Programming");
+	}
+
+}
